2def7c368e01607bb960a05bfb29821421e615ea
5555yxy <go0gle@outlook.com>
2021-01-10 20:17:38 +0800

    删除

--- weather/app/src/main/java/xyz/yxy/weather/CityInfo.java
+++
@@ -1,7 +0,0 @@
-package xyz.yxy.weather;
-public class CityInfo {
-    public String city;//请求城市
-    public String cityId;//请求ID
-    public String  parent;//上级，一般是省份
-    public String  updateTime;//天气更新时间
-}
